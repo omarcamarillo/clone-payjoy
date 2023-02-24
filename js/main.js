7f5bc9dd9c8f7437829f7d76f62f95ad9c24b27d
Omar Camarillo <116605975+omarcamarillo@users.noreply.github.com>
2023-02-24 11:18:55 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ burger.addEventListener("click", () =>{
     burger.classList.toggle("icon__menu-active")
     menu.classList.toggle("menu_active")
 })
+//Header efect
+window.onscroll = function(){
+    scroll = document.documentElement.scrollTop
+    header = document.getElementById("header")
+    if(scroll > 20){
+        header.classList.add("header_mod")
+    }else if(scroll < 20){
+        header.classList.remove("header_mod")
+    }
+}
